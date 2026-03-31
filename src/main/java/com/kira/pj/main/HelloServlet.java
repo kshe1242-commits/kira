222452df49f3f4613886e7e47ba128206386b289
kkmin20200331-max <kkmin20200331@gmil.com>
2026-03-31 17:43:20 +0900

    add]main.jsp

--- src/main/java/com/kira/pj/main/HelloServlet.java
+++ src/main/java/com/kira/pj/main/HelloServlet.java
@@ -1,13 +1,16 @@
 package com.kira.pj.main;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    request.setAttribute("content", "main.jsp");
+    request.getRequestDispatcher("index.jsp").forward(request,response);
     }
 
     public void destroy() {
